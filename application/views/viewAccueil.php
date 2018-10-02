b88780972d9ffdefdcb63f059aee682b22012335
RAZAFINDRATRIMO <tarazafindratrimo@gmail.com>
2018-10-02 09:47:25 +0200

    Creation page d'accueil

---
+++ application/views/viewAccueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HOME</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+
+<header>
+<h2>Prenom Nom Avatar</h2>
+<div class = "Menu">
+    <a href="">Accueil</a>
+    <a href="">Offres</a>
+    <a href="">Demandes</a>
+    <a href="">Echanges</a>
+    <a href="">Deconnexion</a>
+</div>
+
+<h1>Mes Offres</h1>
+Texte
+<h1>Mes demandes </h1>
+Texte
+<h1>Mes deals </h1>
+Texte
+</header>
+<body>
+    
+</body>
+</html>
