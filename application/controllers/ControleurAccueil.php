26154df15add920c4946638d4086a1d16a5f5e6a
RAZAFINDRATRIMO <tarazafindratrimo@gmail.com>
2018-10-02 12:57:22 +0200

    Creation page ControleurAccueil.php

---
+++ application/controllers/ControleurAccueil.php
@@ -0,0 +1,11 @@
+<?php
+
+class ControleurAccueil extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->model('Model_Accueil');
+        $this->load->view('ViewAccueil');
+    }
+}
+?>
