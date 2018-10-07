425fd460d77a3fb5e4f2de0892a5325170e4223a
RAZAFINDRATRIMO <tarazafindratrimo@gmail.com>
2018-10-07 16:11:07 +0200

    ajout d'une template

--- application/views/View_CreationOffreTR.php
+++ application/views/View_CreationOffreTR.php
@@ -1,14 +1,163 @@
-<!DOCTYPE <!DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Création d'une Offre</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+	<title>Formulaire de contact</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="img/airplane.png"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts1/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts1/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor1/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css1/util.css">
+	<link rel="stylesheet" type="text/css" href="css1/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-    
+
+
+	<div class="container-contact100">
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form" action="support.php" method="post">
+				<span class="contact100-form-title">
+					Contactez nous
+				</span>
+
+				<label class="label-input100" for="first-name">Votre identité</label>
+				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Le champ est requis">
+					<input id="first-name" class="input100" type="text" name="Prenom" placeholder="Prénom">
+					<span class="focus-input100"></span>
+				</div>
+				<div class="wrap-input100 rs2-wrap-input100 validate-input" data-validate="Le champ est requis">
+					<input class="input100" type="text" name="Nom" placeholder="Nom">
+					<span class="focus-input100"></span>
+				</div>
+
+				<label class="label-input100" for="email">Votre e-mail</label>
+				<div class="wrap-input100 validate-input" data-validate = "Une e-mail valide est requis: ex@abc.xyz">
+					<input id="email" class="input100" type="text" name="Email" placeholder="Ex. example@email.com">
+					<span class="focus-input100"></span>
+				</div>
+
+				<label class="label-input100" for="phone">Votre numéro de téléphone</label>
+				<div class="wrap-input100">
+					<input id="phone" class="input100" type="text" name="Telephone" placeholder="Ex : 0602145201">
+					<span class="focus-input100"></span>
+				</div>
+
+				<label class="label-input100" for="message">Votre message</label>
+				<div class="wrap-input100 validate-input" data-validate = "Vous n'avez pas écrit">
+					<textarea id="message" class="input100" name="Message" placeholder="Ecrivez nous..."></textarea>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<input class="contact100-form-btn" type="submit" value="Envoyer">
+						
+					</input>
+				</div>
+			</form>
+
+			<div class="contact100-more flex-col-c-m" style="background-image: url('img/bg-01.jpg');">
+				<div class="flex-w size1 p-b-47">
+					<div class="txt1 p-r-25">
+						<span class="lnr lnr-map-marker"></span>
+					</div>
+
+					<div class="flex-col size2">
+						<span class="txt1 p-b-20">
+							Adresse
+						</span>
+
+						<span class="txt3">
+							Paris 13ème, 28 avenue tropicale
+						</span>
+					</div>
+				</div>
+
+				<div class="dis-flex size1 p-b-47">
+					<div class="txt1 p-r-25">
+						<span class="lnr lnr-phone-handset"></span>
+					</div>
+
+					<div class="flex-col size2">
+						<span class="txt1 p-b-20">
+							Appelez nous
+						</span>
+
+						<span class="txt3">
+							06.29.03.15.20
+						</span>
+					</div>
+				</div>
+
+				<div class="dis-flex size1 p-b-47">
+					<div class="txt1 p-r-25">
+						<span class="lnr lnr-envelope"></span>
+					</div>
+
+					<div class="flex-col size2">
+						<span class="txt1 p-b-20">
+							Support général
+						</span>
+
+						<span class="txt3">
+							paradisdesiles@gmail.com
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<script>
+		$(".selection-2").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
 </body>
 </html>
