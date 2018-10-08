c5b6fb7b9369ceeaa956b7ff7ca0910fb2c29127
AUBOIRON <auboiron_sacha@yahoo.fr>
2018-10-08 12:46:23 +0200

    jiji

--- application/views/viewAccueil.php
+++ application/views/viewAccueil.php
@@ -47,7 +47,7 @@
               <a class="nav-link js-scroll-trigger" href="#about">Demandes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="../View_Inscription.php/">Inscription</a>
+              <a class="nav-link js-scroll-trigger" href="<?= ('index.php/ControleurInscription/pageInscription'); ?>">Inscription</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="#contact"></a>
@@ -69,45 +69,7 @@
     </header>
 
 
-    <!-- Footer -->
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <span class="copyright">Copyright &copy; Your Website 2018</span>
-          </div>
-          <div class="col-md-4">
-            <ul class="list-inline social-buttons">
-              <li class="list-inline-item">
-                <a href="#">
-                  <i class="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#">
-                  <i class="fab fa-facebook-f"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#">
-                  <i class="fab fa-linkedin-in"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <ul class="list-inline quicklinks">
-              <li class="list-inline-item">
-                <a href="#"></a>
-              </li>
-              <li class="list-inline-item">
-                <a href="#"></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+    
 
     <!-- Portfolio Modals -->
 
