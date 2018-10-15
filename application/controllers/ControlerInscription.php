8ae2542d1b10f5b47e0492b5048143d10f0a913f
alisonmacedo <alison-macedo@hotmail.fr>
2018-10-15 12:58:04 +0200

    insertion user dans la bdd (faire la fonction pageInscription)

--- application/controllers/ControlerInscription.php
+++ application/controllers/ControlerInscription.php
@@ -2,10 +2,14 @@
 
 class ControlerInscription extends CI_Controller
 {
-    public function pageInscription()
+    public function index()
     {
         $this->load->model('Model_Inscription');
         $this->load->view('View_Inscription');
+        
+    }
+    public function pageInscription()
+    {
         
     }
 }
