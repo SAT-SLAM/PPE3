6972e658ebf4deca665c8c099d41ad741e7a7080
alisonmacedo <alison-macedo@hotmail.fr>
2018-10-19 00:07:32 +0200

    formulaire (en commentaire) template

--- application/views/View_Connexion.php
+++ application/views/View_Connexion.php
@@ -20,7 +20,7 @@
 
 <body>
 
-  <form role="form" method="post" action="<?php echo base_url('ControleurConnexion/accesConnexion'); ?>">
+<form role="form" method="post" action="<?php echo base_url('user/login_user'); ?>">
                         <fieldset>
                             <div class="form-group"  >
                                 <input class="form-control" placeholder="Login" name="login" type="text" required autofocus>
@@ -35,6 +35,38 @@
                         </fieldset>
                     </form>
   
+                    <!--  <div class="signup__container">
+  <div class="container__child signup__thumbnail">
+    
+    <div class="thumbnail__content text-center">
+      <h1 class="heading--primary">Bonjour chèr(e) utilisateur(trice)</h1>
+    </div>
+    
+    <div class="signup__overlay"></div>
+  </div>
+  <div class="container__child signup__form">
+    <form action="#">
+      <div class="form-group">
+        <label for="username">Login</label>
+        <input class="form-control" type="text" name="login" id="login" required />
+      </div>
+      <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <input class="form-control" type="password" name="mdp" id="mdp" required />
+      </div>
+      <div class="m-t-lg">
+        <ul class="list-inline">
+          <li>
+            <input class="btn btn--form" type="submit" id ="connexion" value="Connexion" />
+          </li>
+          <li>
+            <a class="signup__link" href="<//?php echo site_url('ControlerInscription/index') ?>">Vous n'êtes pas encore membre ?</a>
+          </li>
+        </ul>
+      </div>
+    </form>  
+  </div>
+</div> --> 
   
 
 </body>
